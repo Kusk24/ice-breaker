07e92b4cccceb6c437a9cc78c24319858b6a6b51
Kusk24 <winyumg2003a@gmail.com>
2026-04-20 14:50:11 +0700

    grammar wording fixed

--- lib/letter.ts
+++ lib/letter.ts
@@ -2,10 +2,10 @@ export const letter = `Dear Thae Thu (T3 🪱),
 
 You probably didn't expect a letter hidden here, but here it is — just for you.
 
-Even though gifts, letters might not be the first ever you receive from someone, I hope this is the first ever one, because you are too special I just wanted to give something special.
+Even though gifts and letters might not be the first things you receive from someone, I hope this is the first-ever website anyone has made for you, because you are so special and I just wanted to create something as unique as you are.
 
-I want to know more about youuuuuu. Please allow meeeee (∩˃o˂∩)♡,  Don't be too cruelllllll ૮(˶ㅠ︿ㅠ)ა,
- wanna be yoursss 🎶, Ahem"(,,¬﹏¬,,) I mean your most comfortable friend. remember the early bird bestie thing? hahaha
+I want to know more about youuuuuu. Please allow meeeee (∩˃o˂∩)♡. Don't be too cruelllllll ૮(˶ㅠ︿ㅠ)ა,
+wanna be yoursss 🎶. Ahem (,,¬﹏¬,,), I mean your most comfortable friend. Remember the early bird bestie thing? hahaha
 
 With all best wishes,
 wym 🌙`
